c6c3870484f12270a40213b28711aba8c62d4dd5
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2025-06-07 13:20:57 +0200

    Init commit

---
+++ Game/src/Main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Antoine Hanna on 6/7/2025.
+//
+#include "Window/Window.h"
+
+using LittleEngine::Window::Window;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    Window gameWindow = Window(hInstance, 800, 600, L"Game");
+
+    while (gameWindow.ProcessMessages()) {
+        //Game loop
+    }
+
+    return 0;
+}
